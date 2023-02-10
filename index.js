57a5488c92b7091e2d10fc2e4cfa93482cda07a9
JEONG KI MIN <97235034+nimikgnoej@users.noreply.github.com>
2023-02-10 17:00:42 +0900

    AI 답변 받기

--- index.js
+++ index.js
@@ -1,12 +1,30 @@
-const { Configuration, OpenAIApi } = require("openai");
-const configuration = new Configuration({
-    apiKey: "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",
-});
-const openai = new OpenAIApi(configuration);
-const funct = async () => {
-    const response = await openai.createModeration({
-        input: "NO.",
+const send = document.querySelector(".text-submit")
+const text = document.querySelector(".text-input")
+const mainboard = document.querySelector(".main-chat")
+import { Configuration, OpenAIApi } from "openai";
+
+const clickSend = () => {
+    const myText = text.value;
+    const template = `<div class="line">
+        <div class="chatbox chatbox-right">${myText}</div>
+    </div>`
+    mainboard.innerHTML += template;
+    text.value = "";
+
+    const configuration = new Configuration({
+        organization: "org-zeImgnv8EAVoVuLnte5GxkEd",
+        apiKey: "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",
     });
-    console.log(response)
-};
-funct()
+    const openai = new OpenAIApi(configuration);
+    openai.createCompletion({
+        model: "text-davinci-003",
+        prompt: "Say this is a test",
+        max_tokens: 7,
+        temperature: 0,
+    }).then((result) => {
+        console.log(result)
+    });
+
+}
+
+send.addEventListener("click", clickSend);
